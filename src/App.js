2836aa64ad00ddcd485bfb4a7abee84e1fd28c91
Ardahan Dikmedas <arda75.ad@gmail.com>
2022-07-22 18:32:18 +0300

    Amended commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,55 @@
+<<<<<<< HEAD
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        
+        <Routes>
+          <Route path='/about'element={<About />} />          
+          <Route path='/contact'element={<Contact />} />
+          <Route path='/'element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+
+=======
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +75,4 @@ function App() {
 }
 
 export default App;
+>>>>>>> 3b2e9ec3e820ce55ab0dddfd218fe3fb6a994d5c

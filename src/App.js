0d8c59f725a02e3bb1df5a0d3955f78b10cbe2c5
ardahandikmedas <64791848+ardahandikmedas@users.noreply.github.com>
2022-07-22 18:35:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -47,32 +46,3 @@ function About() {
 function Contact() {
   return <h2>Contact</h2>;
 }
-
-
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 3b2e9ec3e820ce55ab0dddfd218fe3fb6a994d5c
